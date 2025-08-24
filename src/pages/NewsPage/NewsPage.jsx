363a4028045b0d692290d8ceb0744cf5f0af05f0
khusniddinovuz <khusniddinovuz@gmail.com>
2025-08-24 18:09:10 +0200

    add date-view to news

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,21 +1,57 @@
 import React, {useEffect, useState} from 'react';
 import "./NewsPage.css";
+import banner from "../../assets/rahbariyat/banner.jpg";
+import bannerVector from "../../assets/rahbariyat/banner-vector.png";
+import bannerBuilding from "../../assets/rahbariyat/banner-building.svg";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import axios from "axios";
 import {NavLink} from "react-router";
-import banner from "../../assets/rahbariyat/banner.jpg";
-import bannerVector from "../../assets/rahbariyat/banner-vector.png";
-import bannerBuilding from "../../assets/rahbariyat/banner-building.svg";
+import ReactPaginate from "react-paginate";
 
 const NewsPage = () => {
   const [news, setNews] = useState([]);
+  const [page, setPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const [currentItems, setCurrentItems] = useState([]);
+  const [start, setStart] = useState(0);
 
   useEffect(() => {
     axios.get('http://localhost:8008/api/articles').then(res => {
       setNews(res.data);
     })
   }, [])
+
+  useEffect(() => {
+    setPageCount(Math.ceil(news.length / 12));
+    setStart(page * 12);
+    setCurrentItems(news.slice(start, start + 12));
+  }, [news, page, start])
+
+  const handlePageClick = ({selected}) => {
+    setPage(selected);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+
+  const formatDate = (dateString) => {
+    const monthsUz = [
+      "Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun",
+      "Iyul", "Avgust", "Sentyabr", "Oktyabr", "Noyabr", "Dekabr"
+    ];
+    const d = new Date(dateString);
+    const time = d.toLocaleTimeString("uz-UZ", {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    });
+    const day = d.getDate().toString().padStart(2, "0");
+    const month = monthsUz[d.getMonth()];
+
+    return ` ${time} - ${day} ${month}`;
+  }
 
   return (
       <div id="news-page">
@@ -54,17 +90,47 @@ const NewsPage = () => {
           </section>
           <div className="news-page-content">
             <div className="news-item-wrapper">
-              {news && news.map((item, index) => (<div className="news-item" key={index}>
-                <NavLink to={"/news"}>
-                  <div className="news-img-wrapper">
-                    <img src={item["image_url"]} className="news-img"
-                         alt=""/>
-                  </div>
-                  <div className="news-title-wrapper">
-                    <p>{item["title"]}</p>
-                  </div>
-                </NavLink>
-              </div>))}
+              {currentItems && currentItems.map((item, index) => (
+                  <div className="news-item" key={index}>
+                    <NavLink to={"/news"}>
+                      <div className="news-img-wrapper">
+                        <img src={item["image_url"]} className="news-img"
+                             alt=""/>
+                      </div>
+                      <div className="news-title-wrapper">
+                        <p>{item["title"]}</p>
+                      </div>
+                      <div className="news-date-wrapper">
+                        <i className="fa-regular fa-calendar"></i>
+                        <p className="news-date">{formatDate(item["created_at"])}</p>
+                        <div className="news-view-wrapper">
+                          <i className="fa-regular fa-eye"></i>
+                          <p>{item["view_count"]}</p>
+                        </div>
+                      </div>
+                    </NavLink>
+                  </div>))}
+            </div>
+            <div className="news-page-pagination-wrapper">
+              <ReactPaginate
+                  previousLabel={<i className="fa-solid fa-chevron-left"></i>}
+                  nextLabel={<i className="fa-solid fa-chevron-right"></i>}
+                  breakLabel="..."
+                  pageCount={pageCount}
+                  marginPagesDisplayed={1}
+                  pageRangeDisplayed={3}
+                  onPageChange={handlePageClick}
+                  containerClassName="pagination"
+                  pageClassName="page-item"
+                  pageLinkClassName="page-link"
+                  previousClassName="page-item"
+                  previousLinkClassName="page-link"
+                  nextClassName="page-item"
+                  nextLinkClassName="page-link"
+                  breakClassName="page-item"
+                  breakLinkClassName="page-link"
+                  activeClassName="active"
+              />
             </div>
           </div>
         </div>
